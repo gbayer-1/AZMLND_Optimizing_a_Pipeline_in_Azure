1d6294705b2dfd3d399ab59cbf2c0170ea3b19c1
gbayer-1 <92030321+gbayer-1@users.noreply.github.com>
2021-10-06 14:11:49 +0200

    Update train.py

--- train.py
+++ train.py
@@ -13,14 +13,20 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-ds = ### YOUR CODE HERE ###
+path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+ds = TabularDatasetFactory.from_delimited_files(path,
+                                                validate=True,
+                                                infer_column_types=True, 
+                                                separator=',', 
+                                                header=True, 
+                                                support_multi_line=False,
+                                                empty_as_string=False,
+                                                encoding='utf8')
 
 x, y = clean_data(ds)
 
 # TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
+x_train, x_test, y_train, y_test = train_test_split(x, y)
 
 run = Run.get_context()
 
